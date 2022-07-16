c5a8cc7e391e0200c87a522f2c74fd2152521e92
Roberto <roherool@hotmail.com>
2022-07-16 17:30:16 -0300

    Created Home page structure and partial styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,34 @@
+import { Play } from 'phosphor-react'
+
 import * as s from './styles'
 
 export function Home() {
   return (
-    <s.Container>
-      <h1>Home</h1>
-    </s.Container>
+    <s.HomeContainer>
+      <form action="">
+        <s.FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" type="text" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input id="minutesAmount" type="number" />
+
+          <span>minutos.</span>
+        </s.FormContainer>
+
+        <s.CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <s.Separator>:</s.Separator>
+          <span>0</span>
+          <span>0</span>
+        </s.CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </s.HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,52 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const HomeContainer = styled.main`
+  align-items: center;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+
+  form {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    gap: 3.5rem;
+  }
+`
+
+export const FormContainer = styled.div`
+  align-items: center;
+  color: ${(props) => props.theme['gray-100']};
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 1.125rem;
+  font-weight: bold;
+  justify-content: center;
+  gap: 0.5rem;
+  width: 100%;
+`
+
+export const CountdownContainer = styled.div`
+  color: ${(props) => props.theme['gray-100']};
+  display: flex;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10rem;
+  gap: 1rem;
+  line-height: 8rem;
+
+  span {
+    background: ${(props) => props.theme['gray-700']};
+    border-radius: 8px;
+    padding: 2rem 1rem;
+  }
+`
+
+export const Separator = styled.div`
+  color: ${(props) => props.theme['green-500']};
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  padding: 2rem 0;
+  width: 4rem;
+`
