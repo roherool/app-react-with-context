d5ffc735f7c998d2f6158c6c92ff60512dc0300e
Roberto <roherool@hotmail.com>
2022-07-16 21:14:54 -0300

    Created History page with complete styling

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -2,8 +2,63 @@ import * as s from './styles'
 
 export function History() {
   return (
-    <s.Container>
-      <h1>Historico</h1>
-    </s.Container>
+    <s.HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <s.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <s.Status statusColor="green">Concluído</s.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <s.Status statusColor="yellow">Em andamento</s.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <s.Status statusColor="green">Concluído</s.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <s.Status statusColor="yellow">Em andamento</s.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <s.Status statusColor="red">Interrompido</s.Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </s.HistoryList>
+    </s.HistoryContainer>
   )
 }
--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,3 +1,85 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const HistoryContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 3.5rem;
+
+  h1 {
+    color: ${(props) => props.theme['gray-100']};
+    font-size: 1.5rem;
+  }
+`
+
+export const HistoryList = styled.div`
+  flex: 1;
+  margin-top: 2rem;
+  overflow: auto;
+
+  table {
+    border-collapse: collapse;
+    min-width: 600px;
+    width: 100%;
+
+    th {
+      background: ${(props) => props.theme['gray-600']};
+      color: ${(props) => props.theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+      padding: 1rem;
+      text-align: left;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+      padding: 1rem;
+
+      &:first-child {
+        padding-left: 1.5rem;
+        width: 50%;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`
+
+const STATUS_COLORS = {
+  green: 'green-500',
+  yellow: 'yellow-500',
+  red: 'red-500',
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  align-items: center;
+  display: flex;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+    border-radius: 50%;
+    height: 0.5rem;
+    width: 0.5rem;
+  }
+`
